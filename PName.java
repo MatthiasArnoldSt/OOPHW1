ea6a08eaa9946afef8dcd5b10a7372d8e5020292
MatthiasArnoldSt <94778053+MatthiasArnoldSt@users.noreply.github.com>
2023-03-06 22:07:46 +0800

    Create PName.java

---
+++ PName.java
@@ -0,0 +1,17 @@
+public class PName{
+    String fname;
+    String lname;
+
+    public PName(String fname, String lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public String getFName(){
+        return fname;
+    }
+
+    public String getLName(){
+        return lname;
+    }
+}
